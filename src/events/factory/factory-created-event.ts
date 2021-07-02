d7eacbb54bc5039ecd0f9fce69d320ba67643771
Patrick Gorman <gormanpe@vcu.edu>
2021-07-01 22:45:07 -0400

    Added id to factory created event

--- src/events/factory/factory-created-event.ts
+++ src/events/factory/factory-created-event.ts
@@ -3,6 +3,7 @@ import {Subjects} from "../subjects";
 export interface FactoryCreatedEvent {
     subject: Subjects.FactoryCreated;
     data: {
+        id: string;
         name: string;
         location: { lat: number, long: number };
         cost: number;
