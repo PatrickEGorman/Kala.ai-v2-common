d2559732c921373c87a2abad6136625afaf9975c
Patrick Gorman <gormanpe@vcu.edu>
2021-07-05 17:40:33 -0400

    Added object option to not found error

--- src/errors/not-found-error.ts
+++ src/errors/not-found-error.ts
@@ -2,13 +2,15 @@ import {CustomError} from "./CustomError";
 
 export class NotFoundError extends CustomError {
     statusCode = 404
+    statement = '';
 
-    constructor() {
-        super('Route not found');
+    constructor(object?: string) {
+        super(`${object ? object : "Route"} not found`);
         Object.setPrototypeOf(this, NotFoundError.prototype)
+        this.statement = object ? `${object} not found` : 'Not found';
     }
 
     serializeErrors() {
-        return [{message: 'Not found'}];
+        return [{message: this.statement}];
     }
 }
