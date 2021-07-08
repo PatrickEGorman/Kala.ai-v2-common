73531fe29b8686e0f420a5928a1953822df902c2
Patrick Gorman <gormanpe@vcu.edu>
2021-07-08 15:55:22 -0400

    Updated ProductUPdatedEvent

--- src/events/product/product-updated-event.ts
+++ src/events/product/product-updated-event.ts
@@ -3,6 +3,7 @@ import {Subjects} from '../subjects';
 export interface ProductUpdatedEvent {
     subject: Subjects.ProductUpdated;
     data: {
+        id: string,
         value: number
     }
 }
